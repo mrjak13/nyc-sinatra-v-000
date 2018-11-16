b1ccd7b0246a5e8cddf24734d49c5d94bbab73e9
Justin Kenney <justinkenney13@gmail.com>
2018-11-16 21:48:53 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,8 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    erb :'figures/index'
+  end
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,10 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
 end
