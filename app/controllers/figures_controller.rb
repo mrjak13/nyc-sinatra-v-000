807b760b65519fd4d16ee1f5957c883f178c493a
Justin Kenney <justinkenney13@gmail.com>
2018-11-16 21:52:16 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,11 @@
 class FiguresController < ApplicationController
   # add controller methods
   get '/figures' do
-    erb :'figures/index'
+    erb :'/figures/index'
+  end
+
+  get '/figure/new' do
+    erb :'/figure/new'
   end
 
 
