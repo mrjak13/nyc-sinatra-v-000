bb032141f5d6eb020b8bdac55e28e649c0618cff
Justin Kenney <justinkenney13@gmail.com>
2018-11-18 16:20:09 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,48 +6,58 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/new' do
-
     @titles = Title.all
     @landmarks = Landmark.all
     erb :'/figures/new'
   end
 
   post '/figures' do
-    # binding.pry
-
-    # FIX REPEATING TITLES AND LAND MARKS IE IF CREATED DONT CREATE AGAIN
-    # fIX IF NO CHECK BOXES SELECTED ERROR / BREAK
     if params[:figure][:name] == ""
       redirect '/figures/new'
     else
-      name = params[:figure][:name]
-      @figure = Figure.create(name: name)
+      @figure = Figure.create(name: params[:figure][:name])
 
-      params[:figure][:title_ids] ? params[:figure][:title_ids].each{|title| @figure.titles << Title.find(title)} : nil
+      params[:figure][:title_ids] ? params[:figure][:title_ids].each{|title| @figure.titles << Title.find(title.gsub("title_", ""))} : nil
 
-      params[:figure][:landmark_ids] ? params[:figure][:landmark_ids].each{|landmark| @figure.landmarks << Landmark.find(landmark)} : nil
+      params[:figure][:landmark_ids] ? params[:figure][:landmark_ids].each{|landmark| @figure.landmarks << Landmark.find(landmark.gsub("landmark_", ""))} : nil
 
       params[:title][:name] != "" ? @figure.titles << Title.create(name: params[:title][:name]) : nil
 
       params[:landmark] != "" ? @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year]) : nil
-
     end
 
-    binding.pry
-
-    # params[:figure][:name] => new figure name, [:title_ids] => new figures selected titles already created, [:landmark_ids] => new figures selected landmarks already created
-    # FOR CREATING NEW TITLE params[:title]
-    # FOR CREATING NEW LANDMARK params[:landmark][:name] & params[:landmark][:year]
     redirect to "/figures/#{@figure.id}"
-
   end
 
   get '/figures/:id' do
-    # raise params.inspect
     @figure = Figure.find(params[:id])
-
     erb :'/figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
 
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    if params[:figure][:name] == ""
+      redirect "/figures/#{@figure.id}/edit"
+    else
+      @figure.update(name: params[:figure][:name])
+
+      params[:figure][:title_ids] ? params[:figure][:title_ids].each{|title| @figure.update(titles: Title.find(title.gsub("title_", "")))} : nil
+
+      params[:figure][:landmark_ids] ? params[:figure][:landmark_ids].each{|landmark| @figure.update(landmarks: Landmark.find(landmark.gsub("landmark_", "")))} : nil
+
+      params[:title][:name] != "" ? @figure.titles << Title.create(name: params[:title][:name]) : nil
+
+      params[:landmark] != "" ? @figure.update(landmarks: Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year])) : nil
+    end
+    redirect to "/figures/#{@figure.id}"
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,10 +1,40 @@
 class LandmarksController < ApplicationController
   # add controller methods
   get '/landmarks' do
+    @landmarks = Landmark.all
     erb :'/landmarks/index'
   end
 
   get '/landmarks/new' do
     erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    if params[:landmark][:name] == ""
+      redirect to '/landmarks/new'
+    else
+      @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    end
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    if params[:landmark][:name] == "" || params[:landmark][:year_completed] == ""
+      redirect to "/landmarks/#{@landmark.id}/edit"
+    else
+      @landmark.update(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    end
+    redirect to "landmarks/#{@landmark.id}"
   end
 end
