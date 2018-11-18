8b6335a37f7951b47e7ce8eda9147e98d9df6ddb
Justin Kenney <justinkenney13@gmail.com>
2018-11-18 14:35:29 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,7 @@
 class FiguresController < ApplicationController
   # add controller methods
   get '/figures' do
+    @figures = Figure.all
     erb :'/figures/index'
   end
 
@@ -12,6 +13,7 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    # binding.pry
 
     # FIX REPEATING TITLES AND LAND MARKS IE IF CREATED DONT CREATE AGAIN
     # fIX IF NO CHECK BOXES SELECTED ERROR / BREAK
@@ -20,18 +22,18 @@ class FiguresController < ApplicationController
     else
       name = params[:figure][:name]
       @figure = Figure.create(name: name)
-      params[:figure][:title_ids].each{|title| @figure.titles << Title.find(title)}
-      params[:figure][:landmark_ids].each{|landmark| @figure.landmarks << Landmark.find(landmark)}
 
-      if params[:title][:name] != ""
-        @figure.titles << Title.create(name: params[:title][:name])
-      end
-      if params[:landmark] != ""
-        @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year])
-      end
+      params[:figure][:title_ids] ? params[:figure][:title_ids].each{|title| @figure.titles << Title.find(title)} : nil
+
+      params[:figure][:landmark_ids] ? params[:figure][:landmark_ids].each{|landmark| @figure.landmarks << Landmark.find(landmark)} : nil
+
+      params[:title][:name] != "" ? @figure.titles << Title.create(name: params[:title][:name]) : nil
+
+      params[:landmark] != "" ? @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year]) : nil
+
     end
 
-    # binding.pry
+    binding.pry
 
     # params[:figure][:name] => new figure name, [:title_ids] => new figures selected titles already created, [:landmark_ids] => new figures selected landmarks already created
     # FOR CREATING NEW TITLE params[:title]
